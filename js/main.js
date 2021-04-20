f55ea92d738c60d42e1e7cc88c2ac0e0bafdf9f7
Jeltock <sinkvane@gmail.com>
2021-04-21 00:54:44 +0600

    modal added

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 "use strict";
 
 var navButton = document.querySelector(".menu__btn");
+var body = document.body;
 navButton.addEventListener("click", function () {
   var navMenu = document.querySelector(".menu");
   var navClose = document.querySelector('.menu__close');
   var container = document.querySelector('.container');
-  var body = document.body;
   navClose.addEventListener('click', function () {
     navMenu.classList.remove("menu__active");
     container.classList.remove("container--active");
@@ -16,6 +16,28 @@ navButton.addEventListener("click", function () {
   container.classList.toggle("container--active");
   navMenu.addEventListener("click", function () {
     event.stopPropagation();
+  });
+});
+var callWidget = document.querySelector('.call__widget');
+callWidget.addEventListener('click', function () {
+  var modalPanel = document.querySelector('.modal__panel');
+  var modalContainer = document.querySelector('.modal__container');
+  var modalClose = document.querySelector('.modal__close-img');
+  modalPanel.classList.toggle('modal__panel--opened');
+  body.classList.add("no-scroll");
+  modalContainer.addEventListener('click', function () {
+    event.stopPropagation();
+  });
+  modalPanel.addEventListener('click', function () {
+    modalPanel.classList.remove('modal__panel--opened');
+    modalContainer.classList.remove('modal__container--opened');
+    body.classList.remove("no-scroll");
+  });
+  modalContainer.classList.toggle('modal__container--opened');
+  modalClose.addEventListener('click', function () {
+    modalPanel.classList.remove('modal__panel--opened');
+    modalContainer.classList.remove('modal__container--opened');
+    body.classList.remove("no-scroll");
   });
 });
 //# sourceMappingURL=main.js.map
